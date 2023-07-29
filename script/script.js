b50004c8273921bfeb329f90232b345f778a5aea
AnnaShp <nyutka_slutskaya@mail.ru>
2023-07-29 15:04:53 +0300

    add catch error func

--- script/script.js
+++ script/script.js
@@ -8,68 +8,91 @@ let error = document.getElementById('err');
 
 btn.addEventListener('click', function (e) {
     e.preventDefault();
+    clearFields();
     search();
 })
 
 // функции поиска людей
 async function findPeople() {
-    const result = await fetch(`https://swapi.dev/api/people/${inputNumber}/`);
-    const people = await result.json();
-    info.textContent = `Name: ` + people.name;
+    const response = await fetch(`https://swapi.dev/api/people/${inputNumber}/`);
+    const people = await response.json();
+    try {
+        if (response.ok && inputNumber <= 10) {
+            info.textContent = 'Name: ' + people.name;
+        }
+        else {
+            error.textContent = 'Ошибка 404: введите число меньше указанного.';
+        }
+    }
+    catch (err) {
+        error.textContent = `Ошибка: ${err.message}`;
+        throw err;
+    }
+    finally {
+        console.log(people);
+    }
 }
 // планет
 async function findPlanet() {
     const response = await fetch(`https://swapi.dev/api/planets/${inputNumber}/`);
     const planets = await response.json();
-    info.textContent = `Planet name: ` + planets.name;
+    try {
+        if (response.ok && inputNumber <= 10) {
+            info.textContent = `Planet name: ` + planets.name;
+        }
+        else {
+            error.textContent = 'Ошибка 404: введите число меньше указанного';
+        }
+    }
+    catch (err) {
+        error.textContent = `Ошибка: ${err.message}`;
+        throw err;
+    }
+    finally {
+        console.log(planets);
+    }
 }
 //фильмов
 async function findFilms() {
     const response = await fetch(`https://swapi.dev/api/films/${inputNumber}/`);
     const films = await response.json();
-    info.textContent = `Movie title: ` + films.title;
+    try {
+        if (response.ok && inputNumber <= 10) {
+            info.textContent = `Movie title: ` + films.title;
+        }
+        else {
+            error.textContent = 'Ошибка 404: введите число меньше указанного';
+        }
+    }
+    catch (err) {
+        error.textContent = `Ошибка: ${err.message}`;
+        throw err;
+    }
+    finally {
+        console.log(films);
+    }
 }
 
 // функция вывода инфо на экран
 function search(id) {
     let selectValue = document.getElementById('select_id').value;
     inputNumber = document.getElementById('number').value;
-    return new Promise((resolve, reject) => {
-        if (!selectValue) {
-            info.textContent = '';
-            error.textContent = ''
-        }
-        else if (selectValue === 'peoples') {
-            findPeople();
-            resolve;
-        }
-        else if (selectValue === 'planets') {
-            findPlanet();
-            resolve;
-        }
-        else if (selectValue === 'films') {
-            findFilms();
-            resolve;
-        }
-        else {
-            reject;
-            error.textContent = 'Ошибка. Введите число меньше указанного';
-        }
-    })
+    if (!selectValue) {
+        error.textContent = 'Выберите вариант из списка';
+    }
+    else if (selectValue === 'peoples') {
+        findPeople();
+    }
+    else if (selectValue === 'planets') {
+        findPlanet();
+    }
+    else if (selectValue === 'films') {
+        findFilms();
+    }
 }
 
-
-// search()
-//     .then(function (people) {
-//         return people;
-//     })
-//     .then(function (planets) {
-//         return planets;
-//     })
-//     .then(function findFilms() {
-//         return films;
-//     })
-//     .catch(error)
-
-
-
+function clearFields() {
+    info.textContent = '';
+    error.textContent = '';
+    console.clear();
+}
