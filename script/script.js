efbcc3586d5652580158eceb81c277508c1bd368
AnnaShp <nyutka_slutskaya@mail.ru>
2023-08-08 15:04:03 +0300

    update js part

--- script/script.js
+++ script/script.js
@@ -5,6 +5,11 @@ let id = inputNumber;
 let btn = document.getElementById('btn_find');
 let info = document.getElementById('info');
 let error = document.getElementById('err');
+const urls = {
+    'peoples': `https://swapi.dev/api/people/`,
+    'planets': `https://swapi.dev/api/planets/`,
+    'films': `https://swapi.dev/api/films/`
+}
 
 btn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -12,82 +17,36 @@ btn.addEventListener('click', function (e) {
     search();
 })
 
-// функции поиска людей
-async function findPeople() {
-    const response = await fetch(`https://swapi.dev/api/people/${inputNumber}/`);
-    const people = await response.json();
+
+async function findData(url, errMessage) {
+    const response = await fetch(url);
+    const data = await response.json();
     try {
-        if (response.ok && inputNumber <= 10) {
-            info.textContent = 'Name: ' + people.name;
+        if (response.ok && data) {
+            info.textContent = `Name: ${data.name}`;
         }
         else {
-            error.textContent = 'Ошибка 404: введите число меньше указанного.';
+            error.textContent = `Ошибка ${response.status}: введите число меньше указанного`;
         }
-    }
-    catch (err) {
-        error.textContent = `Ошибка: ${err.message}`;
+    } catch (err) {
+        error.textContent = `Ошибка: ${errMessage}`;
         throw err;
     }
-    finally {
-        console.log(people);
-    }
-}
-// планет
-async function findPlanet() {
-    const response = await fetch(`https://swapi.dev/api/planets/${inputNumber}/`);
-    const planets = await response.json();
-    try {
-        if (response.ok && inputNumber <= 10) {
-            info.textContent = `Planet name: ` + planets.name;
-        }
-        else {
-            error.textContent = 'Ошибка 404: введите число меньше указанного';
-        }
-    }
-    catch (err) {
-        error.textContent = `Ошибка: ${err.message}`;
-        throw err;
-    }
-    finally {
-        console.log(planets);
-    }
-}
-//фильмов
-async function findFilms() {
-    const response = await fetch(`https://swapi.dev/api/films/${inputNumber}/`);
-    const films = await response.json();
-    try {
-        if (response.ok && inputNumber <= 10) {
-            info.textContent = `Movie title: ` + films.title;
-        }
-        else {
-            error.textContent = 'Ошибка 404: введите число меньше указанного';
-        }
-    }
-    catch (err) {
-        error.textContent = `Ошибка: ${err.message}`;
-        throw err;
-    }
-    finally {
-        console.log(films);
-    }
+    console.log(data);
 }
 
 // функция вывода инфо на экран
-function search(id) {
+function search() {
     let selectValue = document.getElementById('select_id').value;
     inputNumber = document.getElementById('number').value;
     if (!selectValue) {
         error.textContent = 'Выберите вариант из списка';
     }
-    else if (selectValue === 'peoples') {
-        findPeople();
+    else if (urls[selectValue]) {
+        findData(`${urls[selectValue]}${inputNumber}/`, `${selectValue.charAt(0).toUpperCase() + selectValue.slice(1)} name: `);
     }
-    else if (selectValue === 'planets') {
-        findPlanet();
-    }
-    else if (selectValue === 'films') {
-        findFilms();
+    else {
+        error.textContent = 'Некорректный выбор';
     }
 }
 
